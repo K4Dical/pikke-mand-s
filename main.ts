3708befd055ab9dd02aa96d5c226facd53368d83
K4Dical <142780943+K4Dical@users.noreply.github.com>
2023-08-21 09:18:31 +0200

    De første filer til MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,4 @@
+music.ringTone(262)
+basic.forever(function () {
+    music.play(music.createSoundExpression(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+})
